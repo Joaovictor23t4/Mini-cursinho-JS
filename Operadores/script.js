b795276ce03b91228e7f854d367fe0a35be8538f
Reehlimas <reehlimasil08@gmail.com>
2023-06-23 14:12:50 -0300

    término-variaveis

---
+++ Operadores/script.js
@@ -0,0 +1,15 @@
+/* let num1 = 10;
+let num2 = 20;
+console.log("5" === "10");
+
+console.log(5 !== "5"); */
+
+let num3 = 10 < 8;
+    console.log(!num3)
+
+
+let a = "Olá";
+let b = "Mundo";
+
+let concatenacao = a + b;
+console.log(concatenacao)
